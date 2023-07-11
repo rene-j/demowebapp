5f63acfde9dd904816dc1d6de5e66c3dfc9b8ce9
René Jaton <rene.jaton@frontiersin.org>
2023-07-11 09:52:26 +0200

    Added demo webapp project

---
+++ src/DemoWebApp/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DemoWebApp.ViewModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
